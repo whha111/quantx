a6b00f2827ead05ede3e13c39871503a785290c9
whha111 <liaoxingchengaw@gmail.com>
2025-10-24 06:24:31 +0800

    Create Home component for tAdd index pagehe main page

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Welcome to QuantX</h1>
+      <p>Select a strategy to start trading automatically.</p>
+    </div>
+  );
+};
+
+export default Home;
